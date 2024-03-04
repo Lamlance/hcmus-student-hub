ceb46254b6295708fb4a74aabe689daf40bba4f1
Lamlance <lamhoangdien113@gmail.com>
2024-03-04 14:43:04 +0700

    init

---
+++ lib/utils/routes/routes.dart
@@ -0,0 +1,15 @@
+import 'package:boilerplate/presentation/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Routes {
+  Routes._();
+
+  //static variables
+  static const String splash = '/splash';
+  static const String login = '/login';
+  static const String home = '/post';
+
+  static final routes = <String, WidgetBuilder>{
+    home: (BuildContext context) => HomeScreen(),
+  };
+}
