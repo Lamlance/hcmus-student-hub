740a6e0c89daa54753b92479038d528ac5224723
dthgiang <duongthanhgiang.0108@gmail.com>
2024-03-06 03:12:36 +0700

    Draw simple UI for Home Page, Signup and Login Screens

---
+++ lib/presentation/home/styles.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class MyStyles {
+  static const TextStyle homeScreenStyle =
+      TextStyle(color: Colors.pink, decoration: TextDecoration.none);
+}
--- lib/presentation/my_app.dart
+++ lib/presentation/my_app.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
           // theme: _themeStore.darkMode
           //     ? AppThemeData.darkThemeData
           //     : AppThemeData.lightThemeData,
-          theme: AppThemeData.darkThemeData,
+          theme: AppThemeData.lightThemeData,
           routes: Routes.routes,
           // locale: Locale(_languageStore.locale),
           // supportedLocales: _languageStore.supportedLanguages
